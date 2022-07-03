8c49434d153064eff3e96cfa44500f2a4c31b4e6
Gustavo <chavezgustavo813@gmail.com>
2022-07-03 11:09:50 -0500

    first draft of player class

---
+++ src/Player.java
@@ -0,0 +1,137 @@
+import java.time.LocalDateTime;
+
+public class Player
+{
+    // Instance Fields
+    private String name;
+    private String creationDate;
+    private int HP = 50;
+    private int AC = 15;
+    private int STR;
+    private int DEX;
+    private int CON;
+    private Weapon weapon;
+
+    // Constructors
+
+    public Player()
+    {
+        name = "Anonymous";
+        creationDate = LocalDateTime.now().toString();
+        STR = 1;
+        DEX = 1;
+        CON = 1;
+        weapon = new Weapon();
+    }
+
+    public Player(String n, int str, int dex, int con, Weapon w)
+    {
+        name = n;
+        creationDate = LocalDateTime.now().toString();
+        AC += Player.calculateModifier(dex);
+        HP += Player.calculateModifier(AC);
+        STR = str;
+        DEX = dex;
+        CON = con;
+        weapon = w;
+    }
+
+    // Setters
+
+    public void setName(String n)
+    {
+        name = n;
+    }
+
+    public void setHP(int hp)
+    {
+        HP = hp;
+    }
+
+    public void setAC(int ac)
+    {
+        AC = ac;
+    }
+
+    public void setSTR(int str)
+    {
+        STR = str;
+    }
+
+    public void setDEX(int dex)
+    {
+        DEX = dex;
+    }
+
+    public void setCON(int con)
+    {
+        CON = con;
+    }
+
+    public void setWeapon(Weapon w)
+    {
+        weapon = w;
+    }
+
+    // Getters
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public String getCreationDate()
+    {
+        return this.creationDate;
+    }
+
+    public int getHP()
+    {
+        return this.HP;
+    }
+
+    public int getAC()
+    {
+        return this.AC;
+    }
+
+    public int getSTR()
+    {
+        return this.STR;
+    }
+
+    public int getDEX()
+    {
+        return this.DEX;
+    }
+
+    public int getCON()
+    {
+        return this.CON;
+    }
+
+    public Weapon getWeapon()
+    {
+        return this.weapon;
+    }
+
+    // Method(s)
+    public static int calculateModifier(int stat)
+    {
+        return stat - 5;
+    }
+
+    // Override(s)
+    @Override
+    public String toString()
+    {
+        return String.format("Name: %s\nCreation Date: %s\nHP: %d\nSTR: %d\nDEX: %d\nCON: %d\n", name, creationDate,HP, STR, DEX, CON);
+    }
+
+    public static void main(String[] args)
+    {
+        Player Goose = new Player("Goose",10, 10, 10, new Weapon("Broadsword", "1d12", 4));
+
+        System.out.println(Goose);
+    }
+}
